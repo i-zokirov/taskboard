364521ef77250b86c4932c5e58c41dd00837c52a
i-zokirov <izokirov@outlook.com>
2023-01-12 10:16:53 +0100

    dirname fixed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -30,7 +30,7 @@ import {
 
 connectDB();
 const app = express();
-const __dirname = path.resolve();
+const dirname = path.resolve();
 // MIDDLEWARE
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -48,11 +48,9 @@ app.use("/api/projects", projectRouteHandler);
 app.use("/api/projects", taskRouteHandler);
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/frontend/build")));
+    app.use(express.static(path.join(dirname, "/frontend/build")));
     app.get("*", (req, res) => {
-        res.sendFile(
-            path.resolve(__dirname, "frontend", "build", "index.html")
-        );
+        res.sendFile(path.resolve(dirname, "frontend", "build", "index.html"));
     });
 } else {
     app.get("/", (req, res) => {
