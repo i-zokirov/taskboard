60c6daf55da1feae8cbe0182d323219941a87315
i-zokirov <izokirov@outlook.com>
2023-01-23 11:02:34 +0100

    prepped for deployment

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -2,14 +2,11 @@ import { io, Socket } from "socket.io-client";
 import { localhost } from "./config";
 import { ServerToClientEvents, ClientToServerEvents } from "./interfaces";
 
-const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(
-    localhost,
-    {
-        rememberUpgrade: true,
-        transports: ["websocket"],
-        secure: true,
-        rejectUnauthorized: true,
-    }
-);
+const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io("/", {
+    rememberUpgrade: true,
+    transports: ["websocket"],
+    secure: true,
+    rejectUnauthorized: true,
+});
 
 export default socket;
